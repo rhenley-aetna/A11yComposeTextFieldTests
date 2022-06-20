020739f59b2fdab690557d4cedf1d4ad7930be39
rhenley-aetna <henleyr@aetna.com>
2022-06-20 18:36:35 -0400

    Added Modifier.onKeyPreviewEvent -- which changes the problems

--- app/src/main/java/com/example/a11ycomposetextfieldtests/MainActivity.kt
+++ app/src/main/java/com/example/a11ycomposetextfieldtests/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.a11ycomposetextfieldtests
 
+import android.content.Context
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -8,18 +9,18 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.magnifier
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.onPreviewKeyEvent
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.semantics.heading
@@ -39,6 +40,7 @@ import com.example.a11ycomposetextfieldtests.ui.theme.A11yComposeTextFieldTestsT
  * These techniques don't resolve the problem that keyboard Tab and Enter characters are eaten by
  * TextFields as characters to be added to the field value.
  */
+@ExperimentalComposeUiApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,6 +58,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@ExperimentalComposeUiApi
 @Composable
 fun NamePasswordPair() {
     val focusManager = LocalFocusManager.current
@@ -73,15 +76,44 @@ fun NamePasswordPair() {
             style = TextStyle(fontSize = 24.sp, fontWeight = FontWeight.Bold),
         )
         Text(
-            text = "Try to use an external keyboard to shift focus from one text field to the other with Tab or to submit the form with Enter",
-            modifier = Modifier.padding(vertical = 8.dp).fillMaxWidth(),
+            text = "Try to use an external keyboard to shift focus from one text field to the other with Tab or to submit the form with Enter.",
+            modifier = Modifier
+                .padding(vertical = 8.dp)
+                .fillMaxWidth(),
             style = TextStyle(fontSize = 14.sp),
         )
         // Username Field
         TextField(
             value = name.value,
             onValueChange = { name.value = it },
-            modifier = Modifier.padding(vertical = 8.dp).fillMaxWidth(),
+            modifier = Modifier
+                .padding(vertical = 8.dp)
+                .fillMaxWidth()
+                .onPreviewKeyEvent { keyEvent ->
+                    // A forward tab from a prior non-TextField (either no initial focus or from
+                    // the Submit button) or a back tab from the following TextField will cause this
+                    // field to be skipped over. Enter causes onDone() to be called twice.
+                    if (keyEvent.key == Key.Tab) {
+                        if (keyEvent.nativeKeyEvent.isShiftPressed) {
+                            focusManager.moveFocus(FocusDirection.Previous)
+                        } else {
+                            focusManager.moveFocus(FocusDirection.Next)
+                        }
+                        true
+                    } else if (keyEvent.key == Key.Enter || keyEvent.key == Key.NumPadEnter) {
+                        onDone(context, name.value, password.value)
+                        true
+                    } else {
+                        false
+                    }
+                    // The following code is exactly as recommended -- and doesn't work either.
+//                    if (keyEvent.nativeKeyEvent.keyCode == KeyEvent.KEYCODE_TAB) {
+//                        focusManager.moveFocus(FocusDirection.Next)
+//                        true
+//                    } else {
+//                        false
+//                    }
+                },
             label = {
                 Text(text = "Enter your username")
             },
@@ -92,7 +124,7 @@ fun NamePasswordPair() {
                 onNext = {
                     // Pressing Ime button would move the text indicator's focus to the bottom
                     // field, if it exists!
-                    focusManager.moveFocus(FocusDirection.Down)
+                    focusManager.moveFocus(FocusDirection.Next)
                 }
             )
         )
@@ -101,7 +133,33 @@ fun NamePasswordPair() {
         TextField(
             value = password.value,
             onValueChange = { password.value = it },
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .onPreviewKeyEvent { keyEvent ->
+                    // A a forward tab from the prior TextField will cause this field to be skipped
+                    // over, as will a back tab from a later non-input field (the Submit button).
+                    // Enter causes onDone() to be called twice.
+                    if (keyEvent.key == Key.Tab) {
+                        if (keyEvent.nativeKeyEvent.isShiftPressed) {
+                            focusManager.moveFocus(FocusDirection.Previous)
+                        } else {
+                            focusManager.moveFocus(FocusDirection.Next)
+                        }
+                        true
+                    } else if (keyEvent.key == Key.Enter || keyEvent.key == Key.NumPadEnter) {
+                        onDone(context, name.value, password.value)
+                        true
+                    } else {
+                        false
+                    }
+                    // The following code is exactly as recommended -- and doesn't work either.
+//                    if (keyEvent.nativeKeyEvent.keyCode == KeyEvent.KEYCODE_TAB) {
+//                        focusManager.moveFocus(FocusDirection.Next)
+//                        true
+//                    } else {
+//                        false
+//                    }
+                },
             label = {
                 Text(text = "Enter your password")
             },
@@ -110,17 +168,34 @@ fun NamePasswordPair() {
             ),
             keyboardActions = KeyboardActions(
                 onDone = {
-                    Toast.makeText(
-                        context,
-                        "Username: ${name.value}/Password: ${password.value} entered",
-                        Toast.LENGTH_LONG
-                    ).show()
+                    onDone(context, name.value, password.value)
                 }
             )
         )
+
+        Button(
+            onClick = {
+                onDone(context, name.value, password.value)
+            }
+        ) {
+            Text(text = "Submit")
+        }
     }
 }
 
+private fun onDone(
+    context: Context,
+    name: String,
+    password: String
+) {
+    Toast.makeText(
+        context,
+        "Username: '${name}'; Password: '${password}' entered",
+        Toast.LENGTH_SHORT
+    ).show()
+}
+
+@ExperimentalComposeUiApi
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
